685f499a14d5e84ab72ca6afec1322c31e2dd293
ApollineCouturier <62022862+ApollineCouturier@users.noreply.github.com>
2021-02-25 10:16:25 +0100

    Add gallerie and images

---
+++ js/script.js
@@ -0,0 +1,39 @@
+
+$(document).ready(function(){
+
+    // aos (animate on scroll)
+    AOS.init();
+
+    // smooth scroll
+    let links = $('.navbar-nav a.nav-link');
+    links.click(function(e){
+        e.preventDefault();
+        let target = $(this).attr('href');
+        $('html, body').stop().animate({
+            scrollTop: $(target).offset().top
+        }, 700);
+    });
+
+
+    // popup gallery 
+    $(document).ready(function() {
+        $('.popup-gallery').magnificPopup({
+            delegate: 'a',
+            type: 'image',
+            tLoading: 'Loading image #%curr%...',
+            mainClass: 'mfp-img-mobile',
+            gallery: {
+                enabled: true,
+                navigateByImgClick: true,
+                preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+            },
+            image: {
+                tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+                titleSrc: function(item) {
+                    return item.el.find('h2').text() +'<small>Apolline Couturier Gauducheau</small>';
+                }
+            }
+        });
+    });
+
+});
